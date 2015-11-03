56ff1d2a0423151ce2abf0b86b5fe93a3c262f2a
menaka <menaka12350@gmail.com>
2015-11-04 00:00:59 +0530

    Business and resources

--- src/main/java/com/hardwhere/model/Business.java
+++ src/main/java/com/hardwhere/model/Business.java
@@ -2,6 +2,15 @@ package com.hardwhere.model;
 
 /**
  * Created by menaka on 11/3/15.
+ *
+ * Class for holding data about business
+ *
  */
 public class Business {
+
+    private Person owner;
+    private Address address;
+    private String name;
+    //private
+
 }
